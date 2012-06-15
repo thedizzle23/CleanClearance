5aa02f2bc51de234a40c5ec45423dea36f00f2cc
Robert DeNicola <thedizzle23@gmail.com>
2012-06-15 15:15:19 -0400

    initialized commit one

---
+++ CleanDesk/CleanDeskTests/CleanDeskTests.h
@@ -0,0 +1,13 @@
+//
+//  CleanDeskTests.h
+//  CleanDeskTests
+//
+//  Created by Robert DeNicola on 6/15/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CleanDeskTests : SenTestCase
+
+@end
---
+++ CleanDesk/CleanDeskTests/CleanDeskTests.m
@@ -0,0 +1,32 @@
+//
+//  CleanDeskTests.m
+//  CleanDeskTests
+//
+//  Created by Robert DeNicola on 6/15/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "CleanDeskTests.h"
+
+@implementation CleanDeskTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CleanDeskTests");
+}
+
+@end
